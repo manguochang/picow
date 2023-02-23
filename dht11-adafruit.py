4b884d2e4fa34d5f9082d3f0feaef59d32625fb3
manguochang <99720076+manguochang@users.noreply.github.com>
2023-02-23 16:01:44 +0800

    Update dht11-adafruit.py

--- dht11-adafruit.py
+++ dht11-adafruit.py
@@ -72,4 +72,4 @@ def sens_data(data):
     print(' ')
     
 timer = Timer(-1)
-timer.init(period=5000, mode=Timer.PERIODIC, callback = sens_data)
+timer.init(period=15000, mode=Timer.PERIODIC, callback = sens_data)
