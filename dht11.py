5e749b963d2bd51c0da7c58f9d58736643a9b8be
manguochang <99720076+manguochang@users.noreply.github.com>
2023-02-23 14:53:48 +0800

    Create dht11.py

---
+++ dht11.py
@@ -0,0 +1,12 @@
+import machine
+from time import sleep
+from dht import DHT11
+ 
+sensor = DHT11(machine.Pin(4))
+ 
+while True:
+    sensor.measure()
+    temp = sensor.temperature()
+    hum = sensor.humidity()
+    print(f'"Temperature":"{temp}","Humidity":"{hum}"')
+    sleep(2)
